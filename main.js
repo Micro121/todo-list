9453cab15aeaf8a4f37da8a4dc40d02664d7bec4
Sulaiman Marey <adammarey51@gmail.com>
2022-04-18 11:08:54 -0700

    built addtodo function

--- main.js
+++ main.js
@@ -11,3 +11,32 @@ document.addEventListener('DOMContentLoaded', getTodos);
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
 filterOption.addEventListener('click', filterTodo);
+
+function addTodo(event) {
+  // prevent form from submitting
+  event.preventDefault();
+  // to do div
+  const todoDiv = document.createElement('div');
+  todoDiv.classList.add('todo');
+  // create li
+  const newTodo = document.createElement('li');
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add('todo-item');
+  todoDiv.appendChild(newTodo);
+  // add to do to local storage
+  saveLocalTodos(todoInput.value);
+  // check mark button
+  const completedButton = document.createElement('button');
+  completedButton.innerHTML = '<i class ="fas fa-check"></i>';
+  completedButton.classList.add('completed-btn');
+  todoDiv.appendChild(completedButton);
+  // check trash button
+  const trashButton = document.createElement('button');
+  trashButton.innerHTML = '<i class ="fas fa-trash"></i>';
+  trashButton.classList.add('trash-btn');
+  todoDiv.appendChild(trashButton);
+  // append to list
+  todoList.appendChild(todoDiv);
+  // clear to do input value
+  todoInput.value = '';
+}
