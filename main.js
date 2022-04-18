2413d4927abfa2ac227652195e46bdf4da2c9b0b
Sulaiman Marey <adammarey51@gmail.com>
2022-04-18 10:58:34 -0700

    added event listeners

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 const filterOption = document.querySelector('.filter-todo');
+
+// event listeners
+
+document.addEventListener('DOMContentLoaded', getTodos);
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
